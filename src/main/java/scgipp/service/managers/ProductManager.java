aab7cd7c3f9f18665063cd65c86fc9f0e2215fe0
Carlos <carlosmkanda@gmail.com>
2017-10-04 22:02:59 -0300

    teste

--- src/main/java/scgipp/service/managers/ProductManager.java
+++ src/main/java/scgipp/service/managers/ProductManager.java
@@ -1,4 +1,20 @@
 package scgipp.service.managers;
 
+import scgipp.data.hibernate.DBConnection;
+import scgipp.data.hibernate.DBManager;
+import scgipp.service.entities.Product;
+import scgipp.system.log.Log;
+
 public class ProductManager {
+
+    private static DBManager dbManager = DBConnection.manager();
+
+    public static Integer addUser(Product product){
+        Integer id = dbManager.add(product);
+        if(product.getId() != null){
+            Log.show("DATABASE", "Product", "Produto <id = " + product.getId() +
+                    "> foi adicionado ao banco de dados");
+        }
+        return id;
+    }
 }
