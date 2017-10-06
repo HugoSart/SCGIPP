1078f809ec67a33d033be519542d5ecffc1c9ea5
tskira <tskira@hotmail.com>
2017-10-06 20:05:29 -0300

    Implementacao da classe Customer

--- src/main/java/scgipp/service/entities/Customer.java
+++ src/main/java/scgipp/service/entities/Customer.java
@@ -1,8 +1,81 @@
 package scgipp.service.entities;
 
+import br.com.uol.pagseguro.domain.Address;
 import scgipp.data.hibernate.Entity;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by kira on 06/10
+ */
+
+@javax.persistence.Entity
 public class Customer extends Entity {
 
+    @Column(unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Date date;
+
+    @Column(unique = true)
+    private String cpf;
+
+    @Embedded
+    private Address address;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private Tipo tipo;
+
+    protected Customer() {}
+
+    protected Customer(String name, Date date, String cpf, Address address,
+                       String phone, Tipo tipo)
+    {
+        this();
+        setName(name);
+        setDate(date);
+        setCpf(cpf);
+        setAddress(address);
+        setPhone(phone);
+        setTipo(tipo);
+
+    }
+
+    public void setId(Integer id) {this.id = id;}
+
+    public Integer getId() {return id;}
+
+    public void setName(String name) {this.name = name; }
+
+    public String getName() {return name;}
+
+    public void setDate(Date date) {this.date = date;}
+
+    public Date getDate() {return this.date;}
+
+    public void setCpf(String cpf) {this.cpf = cpf;}
+
+    public String getCpf() {return cpf;}
+
+    public void setAddress(Address address) {this.address = address;}
+
+    public Address getAddress() {return address;}
+
+    public void setPhone(String phone) {this.phone = phone;}
+
+    public String getPhone() {return phone;}
+
+    public void setTipo(Tipo tipo) {this.tipo = tipo;}
+
+    public Tipo getTipo() {return this.tipo;}
+
+    public String toString() {return "id = " + id + "name = " + name + "cpf = " + cpf  + ";\n";}
 
 }
