e3659b99a70de65b718bc4bf6bb79f55e28b8fcc
Carlos Kanda <ra94090@uem.br>
2017-11-30 19:58:26 -0200

    Não subiu para o zé.2

--- src/main/java/scgipp/ui/scenarios/AddCartPagSeguro.java
+++ src/main/java/scgipp/ui/scenarios/AddCartPagSeguro.java
@@ -34,7 +34,7 @@ import java.util.Map;
 
 import static javax.swing.UIManager.get;
 
-public class AddCartPagSeguro extends FeedbackScenario {
+public class AddCartPagSeguro extends FeedbackScenario { //para o zé
 
     private ProductManager productManager = ProductManager.getInstance();
     private CartItemManager cartItemManager = CartItemManager.getInstance();
